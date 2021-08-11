89f8c6f31e9442fdc571fa2f72f76593d5f46f02
Justin Heath <just.coder@outlook.com>
2021-08-11 18:03:58 +0100

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-function testJH() {
-    return 'Test'
+const testJH = () => 'temp'
+
+let hello:string = 'world'
+// hello = true
+
+const getFUllName = (first: string, last: string): string => {
+    return first + ' ' + last
 }
+getFUllName('test1', 'test2')
+
+// =================================================
+interface User {
+    name: string;
+    age?: number;
+}
+
+const user: User = {
+    name: 'john',
+    age: 21
+}
+
+const user2: User = {
+    name: 'john',
+}
+// =================================================
 
 export default testJH;
